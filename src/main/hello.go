83d51b9837317fbaf8701439d8222282f3512504
ponted <ponted@ponteddeMacBook-Pro.local>
2018-04-10 10:43:11 +0800

    add

---
+++ src/main/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	//"userHttp"
+	//"userSqlite"
+	"userHttp"
+)
+
+func main (){
+
+	userHttp.WebService()
+
+	//data := userSqlite.Usegr{
+	//	Name:"100",
+	//}
+	//userSqlite.InsertData(data)
+
+	//userSqlite.QueryData()
+
+}
+
