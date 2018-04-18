038837ed71ec742eee8267db91b9f37428266e76
ponted <ponted@ponteddeMacBook-Pro.local>
2018-04-18 19:56:44 +0800

    bug hello

--- src/main/hello.go
+++ src/main/hello.go
@@ -3,12 +3,14 @@ package main
 import (
 	//"userHttp"
 	//"userSqlite"
-	"userHttp"
+	//"userHttp"
+	"fmt"
+	"reflect"
 )
 
 func main (){
 
-	userHttp.WebService()
+	//userHttp.WebService()
 
 	//data := userSqlite.Usegr{
 	//	Name:"100",
@@ -17,5 +19,24 @@ func main (){
 
 	//userSqlite.QueryData()
 
+	u:= user{"hello","12"}
+
+	v:= reflect.TypeOf(u)
+
+	m:="21"
+	x:= reflect.ValueOf(&m)
+	x.Elem().SetString("12")
+
+	fmt.Println(v)
+	fmt.Println(x)
+
 }
+
+//反射
+
+type user struct {
+	Name string
+	Age  string
+}
+
 
